03abe43a2deb94b3cb67cc227670eae7b27aaa89
Mrooski <mileswascher@gmail.com>
2023-12-07 23:41:24 +0000

    direct associations added

--- app/models/trade.rb
+++ app/models/trade.rb
@@ -1,2 +1,5 @@
 class Trade < ApplicationRecord
+  belongs_to :asset, required: true, class_name: "Asset", foreign_key: "asset_id"
+  belongs_to :sender, required: true, class_name: "User", foreign_key: "sender_id"
+  belongs_to :reciever, required: true, class_name: "User", foreign_key: "reciever_id"
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  has_many  :trades_sent, class_name: "Trade", foreign_key: "sender_id", dependent: :nullify
+  has_many  :trades_recieved, class_name: "Trade", foreign_key: "reciever_id", dependent: :destroy
 end
